89929257ac54fa010477ba106a22c9443fe2a429
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-08-22 08:06:03 -0400

    Iniciando a programação do footer

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import Logo from '../../assets/logo-branca.webp'
+function Footer(){
+    return(
+        <footer>
+            <img src={Logo} alt=""/>
+        </footer>
+    )
+}
+export default Footer
