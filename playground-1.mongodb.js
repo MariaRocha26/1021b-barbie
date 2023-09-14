41b116be6e3da3d45590bd95b40da1d9621f6a88
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-09-14 10:11:44 -0400

    atualizando barra de pesquisa

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -29,7 +29,7 @@ db.cardapio.find({nome: "Macarrão"});*/
 
 
 
-use("teretreino");
+/*use("teretreino");
 db.createCollection("treino");
 
 db.treino.insertOne(
@@ -150,7 +150,187 @@ use("teretreino");
 db.treino.updateOne(
     {_id:ObjectId("64f741dd17af774ae34adea0"), "exercicios.nome": "Rosca Scott"}, 
     {$set: {"exercicios.$.serie": "5", "exercicios.$.repeticoes": "12"}}
+)*/
+
+
+/////////// Atividade 1 ///////////////
+use("db_patissier");
+db.createCollection("cake");
+
+/////////// Atividade 2 ///////////////
+use("db_patissier");
+db.cake.insertOne(
+    {
+        _id: ObjectId ("111111111111"),
+        nome: "Bolo de Fubá",
+        peso: "1,5 kg",
+        preco: "50 reais",
+        ingredientes:[
+            {
+                nome:"Ovos",
+                quantidade: "3"
+            },
+            {
+                nome:"Fubá",
+                quantidade: "2 xícaras (chá)"
+            },
+            {
+                nome:"Óleo",
+                quantidade: "1/2 copo (americano)"
+            },
+            {
+                nome:"Fermento em pó",
+                quantidade: "1 colher (sopa)"
+            },
+            {
+                nome:"Açúcar",
+                quantidade: "2 xícaras (chá)"
+            },
+            {
+                nome:"Farinha de trigo",
+                quantidade: "3 colheres (sopa) rasas"
+            },  
+            {
+                nome:"Leite",
+                quantidade: "1 copo"
+            }
+        ]
+    }
 )
+
+db.cake.insertOne(
+    {
+        _id: ObjectId ("222222222222"),
+        nome: "Bolo de Chocolate",
+        peso: "2 kg",
+        preco: "80 reais",
+        ingredientes:[
+            {
+                nome:"Ovos",
+                quantidade: "4"
+            },
+            {
+                nome:"Chocolate em pó",
+                quantidade: "4 colheres (sopa)"
+            },
+            {
+                nome:"Manteiga",
+                quantidade: "2 colheres"
+            },
+            {
+                nome:"Fermento em pó",
+                quantidade: "2 colheres (sopa)"
+            },
+            {
+                nome:"Açúcar",
+                quantidade: "2 xícaras (chá) de açúcar"
+            },
+            {
+                nome:"Farinha de trigo",
+                quantidade: "3 xícaras (chá)"
+            },  
+            {
+                nome:"Leite",
+                quantidade: "1 xícara (chá)"
+            }
+        ]
+    }
+)
+
+db.cake.insertOne(
+    {
+        _id: ObjectId ("333333333333"),
+        nome: "Bolo de Limão",
+        peso: "2,5 kg",
+        preco: "90 reais",
+        ingredientes:[
+            {
+                nome:"Ovos",
+                quantidade: "2 (claras em neve)"
+            },
+            {
+                nome:"Limão",
+                quantidade: "suco de 1 ou 2 limões (depende do tamanho)"
+            },
+            {
+                nome:"Manteiga",
+                quantidade: "2 colheres"
+            },
+            {
+                nome:"Fermento em pó",
+                quantidade: "2 colheres (sopa)"
+            },
+            {
+                nome:"Açúcar",
+                quantidade: "1 e 1/2 xícara"
+            },
+            {
+                nome:"Farinha de trigo",
+                quantidade: "3 e ¹/2 xícaras"
+            },  
+            {
+                nome:"Leite",
+                quantidade: "1 xícara (chá)"
+            }
+        ]
+    }
+)
+
+db.cake.insertOne(
+    {
+        _id: ObjectId ("444444444444"),
+        nome: "Bolo de Red Velvet",
+        peso: "3 kg",
+        preco: "150 reais",
+        ingredientes:[
+            {
+                nome:"Ovos",
+                quantidade: "4"
+            },
+            {
+                nome:"Chocolate em pó",
+                quantidade: "2 e 1/2 colheres (sopa)"
+            },
+            {
+                nome:"Manteiga",
+                quantidade: "3 colheres (sopa)"
+            },
+            {
+                nome:"Fermento em pó",
+                quantidade: "2 colheres (sopa)"
+            },
+            {
+                nome:"Açúcar",
+                quantidade: "1 e 1/4 de xícara"
+            },
+            {
+                nome:"Farinha de trigo",
+                quantidade: "1 xícara e 1/2 colher (sopa)"
+            },  
+            {
+                nome:"Leite",
+                quantidade: "100 ml de leite aquecido"
+            },
+            {
+                nome:"Vinagre branco",
+                quantidade: "1/2 colher (chá)"
+            },
+            {
+                nome:"Corante vermelho em gel",
+                quantidade: "1 e 1/2 colher (sopa)"
+            }
+        ]
+    }
+)
+
+/////////// Atividade 3 ///////////////
+use("db_patissier");
+db.cake.deleteOne({_id:ObjectId("111111111111")});
+
+
+/////////// Atividade 4 ///////////////
+
+
 
 
 
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -49,13 +49,22 @@ export default function Main(){
             
 
         <main className="content-main">
-            <Filme titulo="Barbie" sinopse="Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, 
+            {filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map((filme:FilmesType)=>
+            <Filme key={filme.id}
+                   titulo={filme.titulo}
+                   sinopse={filme.sinopse}
+                   imagem={filme.imagem}
+                   />
+                )
+            }
+            
+            {/* <Filme titulo="Barbie" sinopse="Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, 
             Barbie parte para o mundo humano em busca da verdadeira felicidade."
             imagem="barbie-capa.png"/>
             <Filme titulo="Barbie Castelo de Diamante" sinopse="Liana e Alexa saem da sua cabana humilde para vender flores na vila e acabam conhecendo Melody, a guardiã do Castelo de Diamante. As duas decidem ajudá-la a impedir que a chave do castelo seja roubada por Lídia, uma musa egoísta."
             imagem="castelo-diamante.png"/>
             <Filme titulo="Barbie Escola de Princesas" sinopse="Blair é uma garota simples e órfã que foi sorteada para estudar na exclusiva Escola de Princesas. A escola é um lugar incrível, onde as meninas aprendem dança, etiqueta, arte e outras habilidades indispensáveis a uma princesa. Blair adora suas aulas, suas novas fadinhas assistentes e suas novas amigas, as princesas Hadley e Isla. Porém, quando a malvada Dama Devin desconfia que Blair é a princesa e herdeira desaparecida, ela faz de tudo para impedir que Blair assuma o trono."
-            imagem="escola-de-princesas.png"/>
+            imagem="escola-de-princesas.png"/> */}
         </main>
         </>
     )
